5603b43fbfe2b4c8527624621aa5445ed55bd7e4
Me-2502 <22bce257@nirmauni.ac.in>
2025-04-01 15:40:07 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -9,7 +9,7 @@ const connectDB = async () => {
     //   useNewUrlParser: true,
     //   useUnifiedTopology: true,
     // });
-    const uri = 'mongodb+srv://22bce257:baahubali@cluster0.t6y6ilg.mongodb.net/Chat?retryWrites=true&w=majority';
+    const uri = 'mongodb+srv://22bce257:dl@cluster0.t6y6ilg.mongodb.net/Chat?retryWrites=true&w=majority';
     const client = new MongoClient(uri);
     // cachedDb = await mongoose.connect(process.env.MONGO_URI, {
     //   useNewUrlParser: true,
