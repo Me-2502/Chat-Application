acd777828d94748879b2cd76d91c6bb451986651
Me-2502 <22bce257@nirmauni.ac.in>
2025-04-01 15:33:06 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+const { MongoClient } = require('mongodb');
 const colors = require("colors");
 const dotenv = require("dotenv");
 dotenv.config();
@@ -9,17 +9,19 @@ const connectDB = async () => {
     //   useNewUrlParser: true,
     //   useUnifiedTopology: true,
     // });
-    cachedDb = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 5000,
-      socketTimeoutMS: 45000,
-      maxPoolSize: 10,
-    });
-
-    console.log(`MongoDB Connected: ${cachedDb.connection.host}`.cyan.underline);
-    return cachedDb;
+    const uri = 'mongodb+srv://22bce257:baahubali@cluster0.t6y6ilg.mongodb.net/Chat?retryWrites=true&w=majority';
+    const client = new MongoClient(uri);
+    // cachedDb = await mongoose.connect(process.env.MONGO_URI, {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    //   serverSelectionTimeoutMS: 5000,
+    //   socketTimeoutMS: 45000,
+    //   maxPoolSize: 10,
+    // });
+    // console.log(`MongoDB Connected: ${cachedDb.connection.host}`.cyan.underline);
+    // return cachedDb;
     // console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+    return client;
   } catch (error) {
     console.error(`Error: ${error.message}`.red.bold);
     process.exit(1); // Exit with a non-zero status code to indicate an error
